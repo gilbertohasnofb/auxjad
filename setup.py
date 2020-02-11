e41557c38feccec4a9f0fcd0929b4b65a75d54b0
gilbertohasnofb <gilbertohasnofb@gmail.com>
2020-02-11 22:35:37 +0000

    requires only python 3.6 for Travis (build fails with 3.7)

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ auxjad_classifiers = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3 :: Only',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',

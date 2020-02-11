062defa87ef8d229d55fd08494667e88432a47fe
gilbertohasnofb <gilbertohasnofb@gmail.com>
2020-02-11 21:18:40 +0000

    container_comparator() includes grace notes by default

--- auxjad/tools/container_comparator.py
+++ auxjad/tools/container_comparator.py
@@ -5,7 +5,6 @@ def container_comparator(container1: abjad.Container,
                          container2: abjad.Container,
                          *,
                          include_indicators: bool = False,
-                         include_grace_notes: bool = False,
                          ) -> bool:
     r"""A comparator function returning True when two containers are identical
     and False when they are not.
@@ -59,47 +58,32 @@ def container_comparator(container1: abjad.Container,
 
     ..  container:: example
 
-        By default, this function ignores grace notes, so the containers in the
-        example below are understood to be identical:
+        This function also handles grace notes:
+
+        >>> container1 = abjad.Staff(r"c'4 d'4 e'4 f'4")
+        >>> container2 = abjad.Staff(r"c'4 \grace{d'4} d'4 e'4 f'4")
+        >>> auxjad.container_comparator(container1, container2)
+        False
 
         >>> container1 = abjad.Staff(r"c'4 d'4 e'4 f'4 <g' a'>2 r2")
         >>> container2 = abjad.Staff(r"c'4 \grace{c''4} d'4 e'4 "
         ...                          "f'4 <g' a'>2 r2")
         >>> auxjad.container_comparator(container1, container2)
-        True
-
-    ..  container:: example
-
-        Setting the argument ``include_grace_notes`` to ``True`` forces the
-        function to include grace notes in its comparison. In that case, the
-        containers in the example above are not considered identical any
-        longer:
-
-        >>> container1 = abjad.Staff(r"c'4 d'4 e'4 f'4 <g' a'>2 r2")
-        >>> container2 = abjad.Staff(r"c'4 \grace{c''4} d'4 e'4 "
-        ...                          "f'4 <g' a'>2 r2")
-        >>> auxjad.container_comparator(container1,
-        ...                             container2,
-        ...                             include_grace_notes=True,
-        ...                             )
         False
-
-    ..  container:: example
-
-        When the argument ``include_grace_notes`` is set to ``True``, the
-        function will consider not only a grace note is attached to a given
-        leaf, but also wether the contents of the grace containers are the
-        identical:
 
         >>> container1 = abjad.Staff(r"c'4 \grace{c''4} d'4 e'4 "
         ...                          "f'4 <g' a'>2 r2")
         >>> container2 = abjad.Staff(r"c'4 \grace{c''8} d'4 e'4 "
         ...                          "f'4 <g' a'>2 r2")
-        >>> auxjad.container_comparator(container1,
-        ...                             container2,
-        ...                             include_grace_notes=True,
-        ...                             )
+        >>> auxjad.container_comparator(container1, container2)
         False
+
+        >>> container1 = abjad.Staff(r"c'4 \grace{c''16} d'4 e'4 "
+        ...                          "f'4 <g' a'>2 r2")
+        >>> container2 = abjad.Staff(r"c'4 \grace{c''16} d'4 e'4 "
+        ...                          "f'4 <g' a'>2 r2")
+        >>> auxjad.container_comparator(container1, container2)
+        True
 
     """
     if not isinstance(container1, abjad.Container):
@@ -110,8 +94,6 @@ def container_comparator(container1: abjad.Container,
                         "child class")
     if not isinstance(include_indicators, bool):
         raise TypeError("'include_indicators' must be 'bool'")
-    if not isinstance(include_grace_notes, bool):
-        raise TypeError("'include_grace_notes' must be 'bool'")
 
     leaves1 = [leaf for leaf in abjad.select(container1).leaves()]
     leaves2 = [leaf for leaf in abjad.select(container2).leaves()]
@@ -130,27 +112,11 @@ def container_comparator(container1: abjad.Container,
         if type(leaf1) == abjad.Chord:
             if leaf1.written_pitches != leaf2.written_pitches:
                 return False
-
+        if type(abjad.inspect(leaf1).before_grace_container()) != \
+                type(abjad.inspect(leaf2).before_grace_container()):
+            return False
         if include_indicators:
             if abjad.inspect(leaf1).indicators() != \
                     abjad.inspect(leaf2).indicators():
                 return False
-
-        if include_grace_notes:
-            grace_container1 = abjad.inspect(leaf1).before_grace_container()
-            grace_container2 = abjad.inspect(leaf2).before_grace_container()
-            if grace_container1 and grace_container2:
-                ungraced_container1 = abjad.Container()
-                ungraced_container2 = abjad.Container()
-                for component in grace_container1.components:
-                    ungraced_container1.append(component)
-                for component in grace_container2.components:
-                    ungraced_container2.append(component)
-                if not container_comparator(ungraced_container1,
-                                            ungraced_container2,
-                                            ):
-                    return False
-            elif grace_container1 != grace_container2:
-                return False
-
     return True
--- tests/test_container_comparator.py
+++ tests/test_container_comparator.py
@@ -38,44 +38,31 @@ def test_container_comparator_05():
                                        ) == True
 
 
-# def test_container_comparator_06():
-#    container1 = abjad.Staff(r"c'4 d'4 e'4 f'4 <g' a'>2 r2")
-#    container2 = abjad.Staff(r"c'4 \grace{c''4} d'4 e'4 f'4 <g' a'>2 r2")
-#    assert auxjad.container_comparator(container1, container2) == True
-# TODO: investigate why False
+def test_container_comparator_06():
+   container1 = abjad.Staff(r"c'4 d'4 e'4 f'4 <g' a'>2 r2")
+   container2 = abjad.Staff(r"c'4 \grace{d'4} e'4 f'4 <g' a'>2 r2")
+   assert auxjad.container_comparator(container1, container2) == False
 
 
 def test_container_comparator_07():
     container1 = abjad.Staff(r"c'4 d'4 e'4 f'4 <g' a'>2 r2")
     container2 = abjad.Staff(r"c'4 \grace{c''4} d'4 e'4 f'4 <g' a'>2 r2")
-    assert auxjad.container_comparator(container1,
-                                       container2,
-                                       include_grace_notes=True,
-                                       ) == False
+    assert auxjad.container_comparator(container1, container2) == False
 
 
 def test_container_comparator_08():
-    container1 = abjad.Staff(r"c'4 \grace{c''4} d'4 e'4 f'4 <g' a'>2 r2")
-    container2 = abjad.Staff(r"c'4 \grace{c''4} d'4 e'4 f'4 <g' a'>2 r2")
-    assert auxjad.container_comparator(container1,
-                                       container2,
-                                       include_grace_notes=True,
-                                       ) == True
+    container1 = abjad.Staff(r"c'4 \grace{c''16} d'4 e'4 f'4 <g' a'>2 r2")
+    container2 = abjad.Staff(r"c'4 \grace{c''16} d'4 e'4 f'4 <g' a'>2 r2")
+    assert auxjad.container_comparator(container1, container2) == True
 
 
 def test_container_comparator_09():
     container1 = abjad.Staff(r"c'4 \grace{c''4} d'4 e'4 f'4 <g' a'>2 r2")
     container2 = abjad.Staff(r"c'4 \grace{c''8} d'4 e'4 f'4 <g' a'>2 r2")
-    assert auxjad.container_comparator(container1,
-                                       container2,
-                                       include_grace_notes=True,
-                                       ) == False
+    assert auxjad.container_comparator(container1, container2) == False
 
 
 def test_container_comparator_10():
     container1 = abjad.Staff(r"c'4 \grace{c''4} d'4 e'4 f'4 <g' a'>2 r2")
     container2 = abjad.Staff(r"c'4 \grace{b4} d'4 e'4 f'4 <g' a'>2 r2")
-    assert auxjad.container_comparator(container1,
-                                       container2,
-                                       include_grace_notes=True,
-                                       ) == False
+    assert auxjad.container_comparator(container1, container2) == False

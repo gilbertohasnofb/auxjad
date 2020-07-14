8418f0355d67a8b2e48c6cb7824d83c805c66cc0
gilbertohasnofb <gilbertohasnofb@gmail.com>
2020-07-14 12:05:23 +0100

    version 0.8.1 increment

--- auxjad/__init__.py
+++ auxjad/__init__.py
@@ -54,7 +54,7 @@ from .utilities.underfull_duration import underfull_duration
 
 
 __author__ = "Gilberto Agostinho <gilbertohasnofb@gmail.com>"
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 __all__ = [
     '__author__',
     '__version__',

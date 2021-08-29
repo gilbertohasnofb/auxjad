762d00758a10a1a3f5e9e6f8a61d531927ea27c1
gilbertohasnofb <gilbertohasnofb@gmail.com>
2021-08-29 21:20:43 +0100

    adding html_theme_options to Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -99,6 +99,11 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'canonical_url': 'https://gilbertohasnofb.github.io/auxjad-docs/',
+    'display_version': False,
+    'style_nav_header_background': '#53c1ab',
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
